355baf69edc74a5374b6353d64bb9a082f1e6355
Mservis <mservis_kst@mail.ru>
2016-11-24 21:58:31 +0600

    вход и создать заявку

--- src/main/java/example/SikuliTestTest.java
+++ src/main/java/example/SikuliTestTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
+import org.sikuli.script.Region;
 import org.openqa.selenium.By;
 
 public class SikuliTestTest {
@@ -30,17 +31,37 @@ public class SikuliTestTest {
     @Test
     public void testUntitled() throws Exception {
         driver.manage().window().maximize();
-        driver.navigate().to("https://tender.sk.kz");
+       // driver.navigate().to("https://tender.sk.kz");
+        driver.navigate().to("https://tender.sk.kz/OA_HTML/AppsLocalLogin.jsp");
         // Thread.sleep(3000);
+        //**********************************
         Screen screen = new Screen();
-        Pattern poisk = new Pattern("c:\\forsikuli\\ok.png");
-        screen.wait(poisk);
-        screen.click(poisk);
-       // screen.wait(poisk);
-       // screen.click(poisk);
+        Pattern ok_btn = new Pattern("c:\\forsikuli\\ok.png");
+        Pattern gucalov = new Pattern("c:\\forsikuli\\gucalov.png");
+        Pattern ok2 = new Pattern("c:\\forsikuli\\ok2.png");
+        Pattern inputPass = new Pattern("c:\\forsikuli\\inputPass.png");
+        Pattern dialog = new Pattern("c:\\forsikuli\\dialog.png");
+        Pattern ok3 = new Pattern("c:\\forsikuli\\ok3.png");
+        screen.wait(gucalov);
+        screen.click(gucalov);
+        screen.click(ok_btn);
         driver.findElement(By.id("passwordField")).sendKeys("123456");
         driver.findElement(By.id("SubmitButton")).click();
-        // Thread.sleep(3000);
+        //*********************************
+        driver.findElement(By.id("N11:DraftBid:1")).click();//зайти в заявку
+        driver.findElement(By.id("ContinueBtn")).click();//нажать продолжить
+        //driver.findElement(By.xpath("(//button[@type='submit'])[5]")).click(); //создать ценовое
+       // driver.findElement(By.xpath("//img[@title='Подписать файл']")).click();//подписать файл
+        driver.findElement(By.xpath("//span/a/img")).click();//подписать файл
+        screen.click(ok2);
+       Region window = screen.wait(dialog,10000);
+       window.type(inputPass,"123456");
+        //screen.type(inputPass,"123456");
+        window.click(ok3);
+        window.click(inputPass);
+        window.type(inputPass,"123456");
+        window.click(ok3);
+        Thread.sleep(9000);
         // screen.doubleClick(poisk);
         // Thread.sleep(3000);
 
@@ -87,4 +108,5 @@ public class SikuliTestTest {
             acceptNextAlert = true;
         }
     }
+
 }
